87a73c5e6f071f71bfc2e037ff1577d85d9bafdb
Quoc <phuquoc@redweb.dk>
2020-12-17 16:08:38 +0700

    add layout create project

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import { withTranslation } from "react-i18next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
-import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight";
 
 import ListThumb from "../../components/ListThumb";
 import "./index.scss";
 import List from "./List";
 import Thumb from "./Thumb";
+import ModalComponent from "../../components/Modal";
+import CreateProject from "../../components/CreateProject";
+import { Button } from "react-bootstrap";
 
 class Projects extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isList: true
+      isList: true,
+      showModal: false
     };
   }
 
@@ -26,15 +30,42 @@ class Projects extends React.Component {
 
   }
 
+  _handleShowModal = () => {
+    this.setState({
+      showModal: true
+    })
+  }
+
+  handleModalShow = (s) => {
+    this.setState({ 
+      showModal: false 
+    });
+  };
+
+  bodyModal = () => {
+    return (
+      <CreateProject />
+    )
+  }
+
+  footerModal = () => {
+    return (
+      <Button className="btn btn-success w-100">
+        <span className="me-2">Create project</span>
+        <i><FontAwesomeIcon icon={faChevronRight} /></i>
+      </Button>
+    )
+  }
+
   render() {
     const { t, i18n } = this.props;
-    let { isList } = this.state;
+    let { isList, showModal } = this.state;
 
     return (
       <div className="py-4 px-3">
         <div className="d-flex align-items-center justify-content-between mb-3">
           <h2 className="fs-2">Projects</h2>
-          <a href={void(0)} className="cursor-pointer text-decoration-none btn btn-success">
+          <a href={void(0)} className="cursor-pointer text-decoration-none btn btn-success" onClick={this._handleShowModal}>
             <i><FontAwesomeIcon icon={faPlus} /></i>
             <span className="ps-2">Create project</span>
           </a>
@@ -56,6 +87,14 @@ class Projects extends React.Component {
             }
           </div>
         </div>
+        
+        <ModalComponent
+          header={"Create a new project"}
+          body={this.bodyModal()}
+          footer={this.footerModal()}
+          show={showModal}
+          onHide={() => this.handleModalShow(false)}
+        />
       </div>
     );
   }
