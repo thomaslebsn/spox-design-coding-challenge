65b71db4cea014aada83b4e0200564470b2f7354
Quoc <phuquoc@redweb.dk>
2020-12-28 18:02:01 +0700

    add layout projects

--- src/containers/Projects/ProjectModel/ProjectLeadModel.js
+++ src/containers/Projects/ProjectModel/ProjectLeadModel.js
@@ -1,39 +1,20 @@
-import { FIELD_TYPE } from "../../../constants/FieldType";
-
 class ProjectLeadModel {
-  leads = null;
-
-  constructor(data) {
-    this.leads = data
-      ? Object.keys(data)
-          .map((index) => {
-            return [...Array(data[index])].map((item) => {
-              return new ProjectLeadItemModel(item);
-            });
-          })
-          .reduce((arr, el) => {
-            return arr.concat(el);
-          }, [])
-      : [];
-  }
-
-  toDropdownOptions = () => {
-    return this.leads.map((item, index) => {
-      return item.toDropdownOption();
-    });
-  };
-}
-
-class ProjectLeadItemModel {
   constructor(data) {
     this.id = data.id ?? null;
     this.name = data.name ?? "";
     this.avatarUrl = data.avatar_url ?? "";
   }
 
-  toDropdownOption = () => {
-    return { key: this.id, text: this.name, value: this.id };
-  };
+  getName = () => {
+    return (
+      <>
+        <img src={this.avatarUrl} className="img-avatar me-2" />
+        {this.name}
+      </>
+    )
+  }
 }
 
-export { ProjectLeadModel, ProjectLeadItemModel };
+export {
+  ProjectLeadModel
+};
