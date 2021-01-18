fc142a5499433e7c34140cd769316c2d82f15e24
Quoc <phuquoc@redweb.dk>
2021-01-18 17:59:28 +0700

    update layout personal content

--- src/components/ContentSbarRight/index.jsx
+++ src/components/ContentSbarRight/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { withTranslation } from "react-i18next";
+import { Tab, Row, Nav, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCog } from "@fortawesome/free-solid-svg-icons/faUserCog";
 import { faUserCheck } from "@fortawesome/free-solid-svg-icons/faUserCheck";
 import { faHistory } from "@fortawesome/free-solid-svg-icons/faHistory";
+import "./index.scss";
+
+import ComponentPreviewPersona from "../ComponentPreviewPersona";
 
 class ContentSbarRight extends React.Component {
   constructor(props) {
@@ -15,16 +19,16 @@ class ContentSbarRight extends React.Component {
           icon: faUserCog,
           name: "Persona",
         },
-        {
-          id: 1,
-          icon: faUserCheck,
-          name: "Assign",
-        },
-        {
-          id: 2,
-          icon: faHistory,
-          name: "History",
-        },
+        // {
+        //   id: 1,
+        //   icon: faUserCheck,
+        //   name: "Assign",
+        // },
+        // {
+        //   id: 2,
+        //   icon: faHistory,
+        //   name: "History",
+        // },
       ],
     };
   }
@@ -32,26 +36,41 @@ class ContentSbarRight extends React.Component {
   render() {
     let { data } = this.state;
     return (
-      <div className="position-fixed top-0 end-0 bottom-0 w-80 vh-100 bg-white pd-t-80 shadow z-index-10">
-        <ul className="list-unstyled py-3 px-2">
-          {data.map((value, key) => {
-            return (
-              <li key={key} className="py-3 border-bottom-1">
-                <a
-                  href="#"
-                  className="d-block text-center text-decoration-none"
-                >
-                  <i className="text-black">
-                    <FontAwesomeIcon icon={value.icon} />
-                  </i>
-                  <span className="fs-14 text-black opacity-75 d-block">
-                    {value.name}
-                  </span>
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+      <div className="wr_sbar_right position-fixed top-0 end-0 bottom-0 w-80 vh-100 bg-white pd-t-80 shadow z-index-10">
+        <Tab.Container className="h-auto">
+          <div className="position-relative h-100">
+            <div>
+              <Nav variant="pills" className="flex-column py-3 px-2">
+                {data.map((value, index) => {
+                  return (
+                    <Nav.Item key={index} className="border-bottom-1">
+                      <Nav.Link
+                        eventKey={value.id}
+                        className="d-block text-center text px-0 text-black"
+                      >
+                        <i className="">
+                          <FontAwesomeIcon icon={value.icon} />
+                        </i>
+                        <span className="fs-14 opacity-75 d-block">
+                          {value.name}
+                        </span>
+                      </Nav.Link>
+                    </Nav.Item>
+                  );
+                })}
+              </Nav>
+            </div>
+
+            <Tab.Content className="position-absolute top-0 end-100 w-400 mh-100 border-end-1 overflow-hidden overflow-y-auto">
+              <Tab.Pane
+                eventKey="0"
+                className="wr_tabcontent_right bg-white p-3 h-100"
+              >
+                <ComponentPreviewPersona />
+              </Tab.Pane>
+            </Tab.Content>
+          </div>
+        </Tab.Container>
       </div>
     );
   }
