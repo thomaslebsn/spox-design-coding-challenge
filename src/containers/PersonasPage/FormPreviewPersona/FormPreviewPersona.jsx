2b39aad3666413e1692954f7945704c93db1080f
Quoc <phuquoc@redweb.dk>
2021-03-02 16:10:42 +0700

    Update create file

---
+++ src/containers/PersonasPage/FormPreviewPersona/FormPreviewPersona.jsx
@@ -0,0 +1,38 @@
+import React, { Component, lazy } from "react";
+
+import { observer } from "mobx-react";
+
+import { withPersonaViewModel } from "../PersonaViewModels/PersonaViewModelContextProvider";
+import ContentSbarRight from "../../../components/ContentSbarRight";
+
+const FormPreviewPersona = observer(
+  class FormPreviewPersona extends Component {
+    personaFormViewModel = null;
+
+    constructor(props) {
+      super(props);
+
+      const { viewModel } = props;
+      console.log("ContentFormGenera - Debug View Model Preview");
+      console.log(viewModel);
+
+      this.personaFormViewModel = viewModel
+        ? viewModel.getPersonaFormViewModel()
+        : null;
+
+      console.log("After binding class Preview persona");
+      console.log(this.personaFormViewModel);
+    }
+
+    render() {
+      return (
+        <ContentSbarRight
+        //match={this.props.match}
+        // previewPersonaFormViewModel={this.contentFormViewModel}
+        />
+      );
+    }
+  }
+);
+
+export default withPersonaViewModel(FormPreviewPersona);
