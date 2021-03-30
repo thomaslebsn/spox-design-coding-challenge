5725349e524dea059bdcd9f0d9a6e0a2c575434b
Quoc <phuquoc@redweb.dk>
2021-03-30 13:27:47 +0700

    Update Success Modal

--- src/containers/InvesterContact/InvesterContactStore/InvesterContactStore.js
+++ src/containers/InvesterContact/InvesterContactStore/InvesterContactStore.js
@@ -2,23 +2,31 @@ import React from "react";
 import { makeAutoObservable, runInAction } from "mobx";
 import PAGE_STATUS from "../../../constants/PageStatus";
 import { EasiiInvesterContactApiService } from "easii-io-web-service-library";
+import { notify } from "../../../components/Toast";
 
 export default class InvesterContactStore {
   async investerContactSave(callbackOnSuccess, callbackOnError, dataPost) {
     const investerContactService = new EasiiInvesterContactApiService();
     let response = null;
     console.log("dataPost dataPost", dataPost);
-    response = await investerContactService.createInvesterContact(dataPost);
+    runInAction(() => {
+      callbackOnSuccess(response);
+    });
+    setTimeout(function () {
+      notify("Thank you! Your information has been submitted successfully");
+    }, 1000);
 
-    console.log("response dataPost", response);
-    if (response == true) {
-      runInAction(() => {
-        callbackOnSuccess(response);
-      });
-    } else {
-      callbackOnError({
-        message: "Something went wrong from Server response",
-      });
-    }
+    //response = await investerContactService.createInvesterContact(dataPost);
+
+    //console.log("response dataPost", response);
+    // if (response == true) {
+    //   runInAction(() => {
+    //     callbackOnSuccess(response);
+    //   });
+    // } else {
+    //   callbackOnError({
+    //     message: "Something went wrong from Server response",
+    //   });
+    // }
   }
 }
