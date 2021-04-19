1cc21a74bf391ce4dfc4478d73ab6e28e8266b46
Quoc <phuquoc@redweb.dk>
2021-04-19 18:14:11 +0700

    Update refactor project dashboard

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,36 +1,23 @@
 import React from "react";
 import { withTranslation } from "react-i18next";
-
 import "./index.scss";
-import ProjectStore from "../../containers/ProjectsPage/ProjectStore/ProjectStore";
-
-const projectStore = new ProjectStore();
 
 class Projects extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      getDataProjects: [],
-    };
+    this.state = {};
   }
 
-  componentDidMount = () => {
-    projectStore.fetchProjects((data) => {
-      let dataFilter = data.list.splice(0, 5);
-      this.setState({
-        getDataProjects: dataFilter,
-      });
-    });
-  };
-
   render() {
-    const { t, i18n } = this.props;
+    const { t, i18n, projects } = this.props;
+
+    console.log("projectsprojects 66666");
+    console.log(projects);
 
     return (
       <div className="bg-white p-3">
         <div className="d-flex justify-content-between mb-2">
           <h4>{t("txt_projects")}</h4>
-          {/* <p className="mb-0 fs-14">{t("txt_more")}</p> */}
         </div>
         <div className="py-2 px-3 bg-blue">
           <div className="row">
@@ -43,28 +30,41 @@ class Projects extends React.Component {
           </div>
         </div>
         <div className="px-3">
-          {this.state.getDataProjects.map((value, key) => {
-            return (
-              <div key={key} className="row py-3 border-bottom-1 item_project">
-                <div className="col-9">
-                  <div className="d-flex align-items-center">
-                    <img src={value.logoUrl} alt="" className="img-avatar" />
-                    <span className="ps-3">{value.name}</span>
+          {projects &&
+            projects.map((value, key) => {
+              return (
+                <div
+                  key={key}
+                  className="row py-3 border-bottom-1 item_project"
+                >
+                  <div className="col-9">
+                    <div className="d-flex align-items-center">
+                      {/* <img
+                        src={value.logo}
+                        alt={value.logo}
+                        className="img-avatar"
+                      /> */}
+                      <span>{value.name}</span>
+                    </div>
+                  </div>
+                  <div className="col-3">
+                    <div className="d-flex align-items-center">
+                      <img
+                        src={"/assets/images/avatar-4.png"}
+                        alt=""
+                        className="img-avatar"
+                      />
+                      <span className="ps-3">Peter Stanbridge</span>
+                    </div>
                   </div>
                 </div>
-                <div className="col-3">
-                  <div className="d-flex align-items-center">
-                    <img
-                      src={"/assets/images/avatar-4.png"}
-                      alt=""
-                      className="img-avatar"
-                    />
-                    <span className="ps-3">Peter Stanbridge</span>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
+        <div className="text-end mt-3">
+          <a href="/projects" className="mb-0 text-decoration-underline">
+            Show more
+          </a>
         </div>
       </div>
     );
--- src/containers/Homepage/HomeViewModels/HomeViewModel.js
+++ src/containers/Homepage/HomeViewModels/HomeViewModel.js
@@ -1,8 +1,17 @@
+import ProjectsListViewModel from "../../ProjectsPage/ProjectViewModels/ProjectsListViewModel";
+
 class HomeViewModel {
-  constructor(contentStore) {
-    if (contentStore) {
+  projectListViewModel = null;
+  constructor(homeStore, projectStore) {
+    if (homeStore) {
+      this.projectListViewModel = new ProjectsListViewModel(projectStore);
+
+      console.log("this.projectListViewModel 55555");
+      console.log(this.projectListViewModel);
     }
   }
+
+  getProjectListViewModel = () => this.projectListViewModel;
 }
 
 export default HomeViewModel;
