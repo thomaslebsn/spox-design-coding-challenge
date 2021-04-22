e96c82c40daf7b482712a625a0433aacc5095652
hungnguyen <hungnguyen.ckc@gmail.com>
2021-04-22 15:07:00 +0700

    update code

--- src/components/DamButton/index.jsx
+++ src/components/DamButton/index.jsx
@@ -2,22 +2,32 @@ import React, { Component, lazy } from "react";
 import "./index.scss";
 import Iframe from "react-iframe";
 import { AUTHORIZATION_KEY, AXIOS_CONFIGS } from "easii-io-web-service-library";
+import { io } from "socket.io-client";
 const ModalComponent = lazy(() => import("../../components/Modal"));
+
+const socket = io(AXIOS_CONFIGS.BASE_ENDPOINT_URL + ":9999");
 
 class DamButton extends React.Component {
   constructor(props) {
     super(props);
-
+    this.socket = socket;
     this.state = {
       showModal: false,
     };
   }
 
   handleClick = () => {
+    this.socket.emit(
+      "join room",
+      localStorage.getItem(AUTHORIZATION_KEY.TOKEN_USER)
+    );
+
     this.setState({
       showModal: true,
     });
   };
+
+  componentDidMount = () => {};
 
   closeModal = (s) => {
     this.setState({
@@ -26,9 +36,13 @@ class DamButton extends React.Component {
   };
 
   render() {
+    this.socket.on("response assets", (roomId, data) => {
+      console.log(data);
+    });
+
     const urlDam =
       AXIOS_CONFIGS.BASE_ENDPOINT_URL +
-      "/administrator/index.php?option=com_aesir_dam&view=collection_assets&tmpl=component&token=" +
+      "/administrator/index.php?option=com_aesir_dam&view=easii_dam&token=" +
       localStorage.getItem(AUTHORIZATION_KEY.TOKEN_USER);
     return (
       <>
