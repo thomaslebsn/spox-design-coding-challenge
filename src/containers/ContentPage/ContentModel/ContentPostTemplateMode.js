4a58048e5ac00f44191c6303f6a1e75d3a536ab2
hung <hungnguyen.ckc@gmail.com>
2021-05-31 14:07:27 +0700

    update code

--- src/containers/ContentPage/ContentModel/ContentPostTemplateMode.js
+++ src/containers/ContentPage/ContentModel/ContentPostTemplateMode.js
@@ -1,71 +1,22 @@
+import ContentPostFacebookModel from './ContentPostFacebookModel';
+
 class ContentPostTemplateModel {
   rawData = null;
-  constructor() {
+  data = null;
+  constructor(rawData) {
+    this.rawData = rawData;
     this.data = {
-      facebook: {
-        description: {},
-        images: {},
-        selectedPage:{
-          page1: {
-            pageID: 13123,
-            pageInfo: "dasdasdas"    
-          },
-          page2: {
-            pageID: 1312312321,
-            pageInfo: "dasdasdasxcvxbb"    
-          }    
-        },
-        publishPlan:{
-          page1: {
-            postNow: false,
-            scheduled: {}
-          },
-          page2: {
-            postNow: true,
-            scheduled: false,     
-          }   
-        }
-      }, 
-      linkedIn: {
-        description: {},
-        images: {},
-        selectedPage:{
-          page1: {
-            pageID: 13123,
-            pageInfo: "dasdasdas"    
-          },
-          page2: {
-            pageID: 1312312321,
-            pageInfo: "dasdasdasxcvxbb"    
-          }    
-        },
-        publishPlan:{
-          page1: {
-            postNow: false,
-            scheduled: {
-            }
-          },
-          page2: {
-            postNow: true,
-            scheduled: false,     
-          }   
-        }
-      }, 
+      facebook: null,
+      linkendin: null
     };
   }
 
-  formatData = () => {
-    this.data = []; 
-  }
 
-  static __transformDataToContentCreation = (rawDataDescription, rawDataPublishingPlan) => {
-    const contentPostTemplateModel = new ContentPostTemplateModel();
-    contentPostTemplateModel.rawDataDescription = rawDataDescription;
-    contentPostTemplateModel.rawDataPublishingPlan = rawDataPublishingPlan;
+  transformDataToContentCreation = () => {
+    const facebookModel = new ContentPostFacebookModel(this.rawData);
+    this.data.facebook = facebookModel.transformDataToContentCreation();
 
-    contentPostTemplateModel.formatData();
-
-    return contentPostTemplateModel.data;
+    return this.data;
   }
 ​
 }
