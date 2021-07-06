770c65e4af28aa427ea2dc89908205a61a09d3bb
Nguyen Tien Dat <tiendat@redweb.dk>
2021-07-06 14:53:13 +0700

    update text

--- src/containers/ProfilePage/Layout/AvatarDAM.jsx
+++ src/containers/ProfilePage/Layout/AvatarDAM.jsx
@@ -4,7 +4,7 @@ const AvatarDAM = ({children}) => {
   return (
     <div className='col-3'>
       <label className='form-label mb-3' htmlFor='name'>
-        <span className='text-black opacity-75'>Profile picture</span>
+        <span className='text-black'>Profile picture</span>
       </label>
       <div className='border-da-1 mb-3'>
         {children}
